d2de701ec51827ba8af681b84b4342d439887ab3
Dieter Theiler <dtheiler@tugraz.at>
2015-11-16 15:28:58 +0100

    added solr config and evernote import test email and token

--- sss/entrypoint.sh
+++ sss/entrypoint.sh
@@ -30,6 +30,7 @@ sed -i "s#SSS_MYSQL_PORT#${SSS_MYSQL_PORT}#g" ./sss.conf.yaml
 sed -i "s#SSS_MYSQL_USERNAME#${SSS_DB_USER}#g" ./sss.conf.yaml
 sed -i "s#SSS_MYSQL_PASSWORD#${SSS_DB_PASS}#g" ./sss.conf.yaml
 sed -i "s#SSS_MYSQL_SCHEME#${SSS_MYSQL_SCHEME}#g" ./sss.conf.yaml
+sed -i "s#SSS_SORL_PATH#${SSS_SORL_PATH}#g" ./sss.conf.yaml
 sed -i "s#SSS_AUTH_TYPE#${SSS_AUTH_TYPE}#g" ./sss.conf.yaml
 sed -i "s#SSS_REST_API_PATH#${SSS_REST_API_PATH}#g" ./sss.conf.yaml
 sed -i "s#SSS_TETHYS_USER#${SSS_TETHYS_USER}#g" ./sss.conf.yaml
@@ -38,6 +39,8 @@ sed -i "s#SSS_TETHYS_LAS_USER#${SSS_TETHYS_LAS_USER}#g" ./sss.conf.yaml
 sed -i "s#SSS_TETHYS_LAS_PASSWORD#${SSS_TETHYS_LAS_PASSWORD}#g" ./sss.conf.yaml
 sed -i "s#SSS_TETHYS_OIDC_CONF_URI#${SSS_OIDC_CONFIG}#g" ./sss.conf.yaml
 sed -i "s#SSS_TETHYS_OIDC_USER_END_POINT_URI#${SSS_OIDC_ENDPOINT}#g" ./sss.conf.yaml
+sed -i "s#SSS_EVERNOTE_TEST_AUTH_TOKEN#${SSS_EVERNOTE_TEST_AUTH_TOKEN}#g" ./sss.conf.yaml
+sed -i "s#SSS_EVERNOTE_TEST_EMAIL#${SSS_EVERNOTE_TEST_EMAIL}#g" ./sss.conf.yaml
 echo "done --> setting SSS config ..."
 
 echo "sss conf printout ...."
